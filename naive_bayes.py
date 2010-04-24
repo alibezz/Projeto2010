53b04b2cb0abd53521b4a85154cde58ac4d37f29
Alibezz <alibezz@gmail.com>
2010-04-24 20:54:41 -0300

    Refactoring naive bayes

--- naive_bayes.py
+++ naive_bayes.py
@@ -2,6 +2,7 @@ import numpy as np
 import random
 import os
 import sys
+import math
 
 import re
 wre = re.compile(r"[a-zA-Z0-9 ]+")
@@ -19,52 +20,38 @@ def get_words(text):
 
 class NaiveBayesSampler(object):
   def __init__(self):
-    self.c0 = []
-    self.c1 = []
-    self.Nc0 = 0
-    self.Nc1 = 0
-
-    self.labels = {} 
+    self.labels = [] 
     self.docs = []
+    self.bags = []
     
     self.all_words = []
     self.Nwords = 0
-    self.docs_freqs = []
-    self.reverse_map = {}
 
+#OK
   def load_as_bag(self,doc):
         "Creates a bag of words for a single document"
-        v = []
         word_freqs = {}
+        #Talvez self.docs fique desnecessario
         for w in get_words(file("/home/alibezz/Desktop/Projeto2010/vectors/"+doc).read().split(" ")):
             w = w.lower()
             if not w in word_freqs:
               word_freqs[w] = 1
+
             else:
               word_freqs[w] += 1
-            if not w in self.reverse_map:
-                self.reverse_map[w] = self.Nwords
-                self.all_words.append(w)
-                self.Nwords += 1
-            v.append(self.reverse_map[w])
-        self.docs.append(v)
-        self.docs_freqs.append(word_freqs)
+            if not w in self.all_words:
+              self.all_words.append(w)
+              self.Nwords += 1
+        self.docs.append(word_freqs)
 
+#OK
   def pick_label(self):
     return np.random.binomial(1, self.pi)
 
+#OK
   def label_documents(self):
-    for index,d in enumerate(self.docs):
-      label = self.pick_label()
-      #FIXME Change if/else to metaprogramming
-      self.labels[index] = label
-      if label == 0:
-        self.c0.append(index)
-      else:
-        self.c1.append(index)
-
-    self.Nc0 = len(self.c0)
-    self.Nc1 = len(self.c1)
+    for i in xrange(len(self.docs)):
+      self.labels.append(self.pick_label())
 
   def remove_document(self, doc_index):
     wclass = self.labels[doc_index]
@@ -75,6 +62,12 @@ class NaiveBayesSampler(object):
     else:    
       self.c1.remove(doc_index)
       self.Nc1 -= 1
+  
+  def makebag(self, i):
+    bag = []
+    for word in self.all_words:
+      bag.append(self.docs[i][word])
+    return bag  
 
   def word_in_class_frequency(self, wclass, word):
     word_count = 0
@@ -102,27 +95,51 @@ class NaiveBayesSampler(object):
     new_pi = value1 / (value0 + value1)
     return np.random.binomial(1, new_pi)
 
-  def classify_document(self, index):
-    "almost there"
+  def classify_document(self, index, new_label):
+    #FIXME Change if/else to metaprogramming
+    if new_label == 0:
+      self.c0.append(index)
+      self.Nc0 += 1
+    else:
+      self.c1.append(index)
+      self.Nc1 += 1
+    self.labels[index] = new_label
 
   def iterate(self):
     for index in range(len(self.docs)):
       #considering a non-supervised environment
       self.remove_document(index)
       new_label = self.sample_label()
-      self.classify_document(index)
- 
+      self.classify_document(index, new_label)
+    return self.labels.copy()
+
   def sample(self, nsamples):
     #initialize
+    self.Gammapi = np.array([1., 1.])
+    self.Gammatheta = np.array([1. for i in self.all_words])
     self.pi = random.betavariate(1,1)
     self.label_documents()
+    self.wccs = np.zeros((2, len(self.all_words)))
+    self.dccs = np.zeros(2)
+    for i in xrange(len(self.labels)):
+      self.bags.append(self.makebag(i))
+    for i, a in enumerate(self.labels):
+      self.dccs[a] += 1
+      self.wccs[a] += self.bags[i]
     ####
-    samples = []
-    #while len(samples) < 2: #nsamples:
-    self.iterate()
-  
+   
+
+   # samples = []
+   # 
+   # while len(samples) < nsamples:
+   #   it = self.iterate()
+   #   samples.append(it)
+   #   if len(samples) % 10 == 0:
+   #     print it
+   #     print '\n'
+
 if __name__=='__main__':
   docs = os.listdir(sys.argv[1])
   s = NaiveBayesSampler()
   [s.load_as_bag(x) for x in docs]
-  s.sample(10)
+  print s.sample(1000)
