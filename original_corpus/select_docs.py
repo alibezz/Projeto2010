8b46497917e2ba7c2749a147db1b29c2af21990a
Alibezz <alibezz@gmail.com>
2010-05-23 21:09:54 -0300

    Changing to class

--- original_corpus/select_docs.py
+++ original_corpus/select_docs.py
@@ -5,30 +5,34 @@
 import os
 import sys
 
-def author_name(string):
-  ind = string.find("with")
-  if ind >= 0:
-    return string[ind + 5:]
-  else:
-    return string
+class SelectDocs(object):
+  def __init__(self, dir, corpus):
+    self.dir = dir
+    self.corpus = corpus
+    self.relation = {}
 
-if __name__=='__main__':
-  meta_dir = os.listdir(sys.argv[1])
-  relation = {}; view = ""
-  
-  for file in meta_dir:
-    for ind, line in enumerate(open(os.path.realpath(sys.argv[1] + '/' + file), 'r')):
-      columns = line.split("\t")
-      if columns[3] == "meta_viewpoint":
-        view = columns[4].strip("\n")
-      elif columns[3] == "meta_author":
-        author = author_name(columns[4]).strip("\n")
-        if not author in relation:
-          relation[author] = [[file, view]] 
-        else:
-          relation[author].append([file, view])
+  def author_name(self, string):
+    ind = string.find("with")
+    if ind >= 0:
+      return string[ind + 5:]
+    else:
+      return string
 
-  print relation 
-     
-
+  def generate_relations(self):
+    view = ""
+    for file in self.corpus:
+      for ind, line in enumerate(open(os.path.realpath(self.dir + '/' + file), 'r')):
+        columns = line.split("\t")
+        if columns[3] == "meta_viewpoint":
+          view = columns[4].strip("\n")
+        elif columns[3] == "meta_author":
+          author = self.author_name(columns[4]).strip("\n")
+          if not author in self.relation:
+            self.relation[author] = [[file, view]] 
+          else:
+            self.relation[author].append([file, view])
+    print self.relation 
  
+if __name__=='__main__':
+  a = SelectDocs(sys.argv[1], os.listdir(sys.argv[1]))
+  a.generate_relations()
