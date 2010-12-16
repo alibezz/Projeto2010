c7b8c8dc45a4ac148b3e8087af5ec348cc5f2596
alibezz <alibezz@gmail.com>
2010-12-16 02:02:51 -0200

    cleaning up

--- persp_point_of_view/luis_nassif/downloader.py
+++
@@ -1,7 +0,0 @@
-import os
-
-for i in xrange(50):
-  os.system("wget http://www.advivo.com.br/categoria/blogs/politica/eleicoes?page=" + str(i + 1)) 
-#  os.system(cmd)
-#  os.system("mv www.rodrigovianna.com.br pagina" + str(i + 1))
-  
--- persp_point_of_view/luis_nassif/executa.py
+++
@@ -1,7 +0,0 @@
-import os
-
-j = 20
-for i in xrange(35):
-  os.system("python scrape_luis.py ../eleicoes\?page\=" + str(i + 16) + " " + str(280 + j))
-  j += 20
-
